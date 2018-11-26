53bcef513e1c2a37c5108c8f2460a0afc3f654ca
Peter Kreppein <pmkreppein@gmail.com>
2018-11-26 22:12:32 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,19 @@
-# Create your costume_stores migration here
+# Create your costume_stores migration 
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |x|
+      x.string :name
+      x.string :location 
+      x.integer :costume_inventory
+      x.integer :num_of_employees
+      x.boolean :still_in_business
+      x.datetime :opening_time
+      x.datetime :closing_time
+      
+      x.timestamps null: false
+    end
+  
+  end
+  
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |m|
+      m.string :name
+      m.string :location
+      m.float :price
+      m.string :theme
+      m.boolean :family_friendly
+      m.datetime :opening_date
+      m.datetime :closing_date
+      m.text :description
+      
+      m.timestamps null: false
+    end
+  end 
+end
